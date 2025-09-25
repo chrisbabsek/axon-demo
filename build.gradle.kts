dd0ec633bb182e193ace3660911847799dc0efef
Christian Babsek <chrisbabsek@users.noreply.github.com>
2025-09-25 12:18:40 +0200

    chore: remove Gradle wrapper binary

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("com.github.ben-manes.versions") version "0.50.0"
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
-    kotlin("plugin.jpa") version "1.9.20"
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20"
+    kotlin("plugin.jpa") version "2.2.20"
 }
 
 repositories {
@@ -15,7 +16,7 @@ repositories {
 
 group = "de.babsek.demo.axontesting"
 
-extra["axonVersion"] = "4.9.1"
+extra["axonVersion"] = "4.9.4"
 
 dependencyManagement {
     imports {
@@ -31,27 +32,27 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.github.microutils:kotlin-logging:2.0.6")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
 
     implementation("org.axonframework:axon-spring-boot-starter")
-    implementation("org.axonframework.extensions.kotlin:axon-kotlin:4.9.0")
-    implementation("org.axonframework.extensions.kotlin:axon-kotlin-test:4.9.0")
+    implementation("org.axonframework.extensions.kotlin:axon-kotlin:4.12.0")
+    implementation("org.axonframework.extensions.kotlin:axon-kotlin-test:4.12.0")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    runtimeOnly("org.postgresql:postgresql:42.7.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
     implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
     testImplementation("org.axonframework:axon-test")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
